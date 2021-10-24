677ad845d4ac91e47692b430c35ef3ae24ab8727
salihclskan <92811280+salihclskan@users.noreply.github.com>
2021-10-24 17:20:29 +0300

    Create Odev4.sql

---
+++ Odev4.sql
@@ -0,0 +1,5 @@
+--SELECT DISTINCT replacement_cost FROM film ;
+--SELECT COUNT(DISTINCT replacement_cost) FROM film ; --21
+--SELECT COUNT(*) FROM film WHERE (title LIKE 'T%') AND (rating = 'G') ; --9
+--SELECT COUNT(*) FROM country WHERE country LIKE '_____' ; --13
+--SELECT COUNT(*) FROM city WHERE city ILIKE '%r' 
