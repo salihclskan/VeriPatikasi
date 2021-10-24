bbae0b11497345b19a9efb1131ccb83d54099e15
salihclskan <92811280+salihclskan@users.noreply.github.com>
2021-10-24 18:44:17 +0300

    Create Odev6.sql

---
+++ Odev6.sql
@@ -0,0 +1,4 @@
+--SELECT AVG(rental_rate) FROM film; --2,98
+--SELECT COUNT(*) FROM film WHERE title LIKE 'C%'; --92
+--SELECT MAX(length) FROM film WHERE rental_rate = 0.99 ; --184
+--SELECT COUNT(DISTINCT replacement_cost) FROM film WHERE length>150 ; --21
