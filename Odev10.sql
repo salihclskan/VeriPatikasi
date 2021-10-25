1bcb44b743c3009003337caeca78fbd1432b0c8a
salihclskan <92811280+salihclskan@users.noreply.github.com>
2021-10-25 16:38:35 +0300

    Create Odev10.sql

---
+++ Odev10.sql
@@ -0,0 +1,3 @@
+--SELECT city,country FROM country LEFT JOIN city ON city.country_id = country.country_id;
+--SELECT payment_id,first_name,last_name FROM customer RIGHT JOIN payment ON customer.customer_id = payment.customer_id;
+--SELECT rental_id,first_name,last_name FROM rental FULL JOIN customer ON customer.customer_id = rental.customer_id ;
